a7d75993a8e9131820022ef42165a9cfb0acdca7
Pete Lazuran <pdlazuran@gmail.com>
2017-08-25 15:57:37 -0700

    finish styling

---
+++ src/PeteLazuran/wwwroot/js/JavaScript.js
@@ -0,0 +1,14 @@
+﻿//function setHalfVolume() {
+//    var myAudio = document.getElementById("audio1");
+//    myAudio.volume = 0.5;
+//}
+
+//function setHalfVolume() {
+//    var myAudio = document.getElementById("audio2");
+//    myAudio.volume = 0.5;
+//}
+
+//function setHalfVolume() {
+//    var myAudio = document.getElementById("audio3");
+//    myAudio.volume = 0.5;
+//}
